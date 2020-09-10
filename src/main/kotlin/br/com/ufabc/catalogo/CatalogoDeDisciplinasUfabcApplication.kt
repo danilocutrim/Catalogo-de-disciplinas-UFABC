0d611439e54f3811609c5189ef5a29ad4c054826
Danilo Cutrim <danilocutrim.dc@gmail.com>
2020-09-09 22:09:18 -0300

    Adicionados arquivos iniciais de projeto

---
+++ src/main/kotlin/br/com/ufabc/catalogo/CatalogoDeDisciplinasUfabcApplication.kt
@@ -0,0 +1,11 @@
+package br.com.ufabc.catalogo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CatalogoDeDisciplinasUfabcApplication
+
+fun main(args: Array<String>) {
+	runApplication<CatalogoDeDisciplinasUfabcApplication>(*args)
+}
